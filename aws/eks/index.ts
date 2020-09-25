56e9d7df3ab2990f9a19ec9056f95fdc4c94ca84
Lee Briggs <lee@leebriggs.co.uk>
2020-09-24 17:07:13 -0700

    add EKS clusters

---
+++ aws/eks/index.ts
@@ -0,0 +1,30 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
+import * as eks from "@pulumi/eks";
+
+let config = new pulumi.Config()
+const stack = pulumi.getStack()
+const name = `lbriggs-${stack}`
+const vpc = new pulumi.StackReference(`jaxxstorm/aws-vpc/${stack}`);
+const vpcId = vpc.getOutput("vpcId")
+const privateSubnets = vpc.getOutput("privateSubnets")
+const publicSubnets = vpc.getOutput("publicSubnets")
+
+// Define the AWS provider credential opts to configure the cluster's
+// kubeconfig auth.
+const kubeconfigOpts: eks.KubeconfigOptions = {profileName: "personal"};
+
+const cluster = new eks.Cluster(name, {
+    providerCredentialOpts: kubeconfigOpts,
+    name: name,
+    vpcId: vpcId,
+    privateSubnetIds: privateSubnets,
+    publicSubnetIds: publicSubnets,
+    createOidcProvider: true,
+})
+
+export const clusterName = cluster.eksCluster.name
+export const kubeconfig = cluster.kubeconfig
+export const clusterOidcProvider = cluster.core.oidcProvider?.url
+export const clusterOidcProviderArn = cluster.core.oidcProvider?.arn
--- aws/vpc/index.ts
+++ aws/vpc/index.ts
@@ -2,11 +2,24 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-const vpc = new awsx.ec2.Vpc("lbriggs", {
+const stack = pulumi.getStack()
+const name = `lbriggs-${stack}`
+const tagName = `kubernetes.io/cluster/${name}`
+
+// get config values
+let config = new pulumi.Config()
+let cidrBlock = config.get("cidrBlock")
+
+const vpc = new awsx.ec2.Vpc(name, {
     subnets: [
-        { type: "public", tags: { "kubernetes.io/cluster/lbriggs": "shared", "kubernetes.io/role/elb": "1" } },
-        { type: "private", tags: { "kubernetes.io/cluster/lbriggs": "shared", "kubernetes.io/role/internal-elb": "1" }  },
-    ]
+        { type: "public", tags: { tagName: "shared", "kubernetes.io/role/elb": "1" } },
+        { type: "private", tags: { tagName: "shared", "kubernetes.io/role/internal-elb": "1" }  },
+    ],
+    tags: {
+        "Name": name,
+        "Owner": "lbriggs",
+    },
+    cidrBlock: cidrBlock
 })
 
 export const vpcId = vpc.id
