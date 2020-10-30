b9886d0493f39623b6ed8c70749e0a59d7d935a7
Lee Briggs <lee@leebriggs.co.uk>
2020-10-30 14:19:00 -0700

    update eks deployment

--- aws/eks/index.ts
+++ aws/eks/index.ts
@@ -6,17 +6,17 @@ import * as eks from "@pulumi/eks";
 let config = new pulumi.Config()
 const stack = pulumi.getStack()
 const name = `lbriggs-${stack}`
-const vpc = new pulumi.StackReference(`jaxxstorm/aws-vpc/${stack}`);
-const vpcId = vpc.getOutput("vpcId")
+const vpc = new pulumi.StackReference(`jaxxstorm/vpc/${stack}`);
+const vpcId = vpc.getOutput("id")
 const privateSubnets = vpc.getOutput("privateSubnets")
 const publicSubnets = vpc.getOutput("publicSubnets")
 
 // Define the AWS provider credential opts to configure the cluster's
 // kubeconfig auth.
-// const kubeconfigOpts: eks.KubeconfigOptions = {profileName: "personal"};
+const kubeconfigOpts: eks.KubeconfigOptions = {profileName: "pulumi-dev-sandbox"};
 
 const cluster = new eks.Cluster(name, {
-    // providerCredentialOpts: kubeconfigOpts,
+    providerCredentialOpts: kubeconfigOpts,
     name: name,
     vpcId: vpcId,
     privateSubnetIds: privateSubnets,
